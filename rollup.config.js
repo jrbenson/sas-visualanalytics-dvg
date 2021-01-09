cfa20f18f17f357e87795d7f32c02e6348549ee4
Riley Benson <Riley.Benson@sas.com>
2021-01-09 14:49:37 -0500

    Updated build process.

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,27 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
+import clean from 'rollup-plugin-clean'
 
 export default [
   {
-    input: './lib/index.js',
-    output: {
-      file: 'dist/vadynsvg.js',
-      name: 'dynsvg',
-      format: 'iife',
-    },
-    plugins: [nodeResolve(), commonjs()],
-  },
-  {
-    input: './lib/index.js',
-    output: {
-      file: 'dist/vadynsvg.min.js',
-      name: 'dynsvg',
-      format: 'iife',
-    },
-    plugins: [nodeResolve(), terser(), commonjs()],
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/vadynsvg.min.js',
+        name: 'dynsvg',
+        format: 'iife',
+      },
+      {
+        file: 'dist/index.es.js',
+        format: 'es',
+      },
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+      },
+    ],
+    plugins: [clean(), typescript(), nodeResolve(), commonjs(), terser()],
   },
 ]
